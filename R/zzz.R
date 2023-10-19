38462c561258c70c1344ca566b00d2079603df2b
Aaron Chafetz <achafetz@usaid.gov>
2023-10-19 17:32:15 -0400

    feat: add check to gagglr on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,14 @@
 .onAttach <- function(...) {
+
+  oha_check("gagglr", suppress_success = TRUE)
+
   needed <- core[!is_attached(core)]
   available <- core[vapply(needed, requireNamespace, quietly = TRUE, FUN.VALUE = logical(1))]
   if (length(available) == 0)
     return()
 
   crayon::num_colors(TRUE)
-  # oha_attach()
+  oha_attach()
 }
 
 is_attached <- function(x) {
